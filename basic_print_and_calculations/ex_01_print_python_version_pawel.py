805505e6dbbe0250062347c07a2d7dea53e07de5
azharmhk <makpro801612@gmail.com>
2024-09-19 16:08:27 +0530

    add ex_1 file

---
+++ basic_print_and_calculations/ex_01_print_python_version_pawel.py
@@ -0,0 +1,12 @@
+"""
+Exercise 0
+Print the Python version to the console.
+
+Expected result: 3.8.19
+"""
+
+import sys
+
+print(sys.version)
+print(sys.version.split())
+print(sys.version.split()[0])
